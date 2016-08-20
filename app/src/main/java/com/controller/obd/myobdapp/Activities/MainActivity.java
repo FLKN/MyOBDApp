e58462ebd86e1341fd85688721dfd5c7d76fe2cf
David Pérez Espino <dpereze1105@hotmail.com>
2016-08-19 23:41:17 -0500

    Starting Bluetooth

--- app/src/main/java/com/controller/obd/myobdapp/Activities/MainActivity.java
+++ app/src/main/java/com/controller/obd/myobdapp/Activities/MainActivity.java
@@ -1,7 +1,11 @@
 package com.controller.obd.myobdapp.Activities;
 
+import android.app.ProgressDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
-import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -13,14 +17,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.controller.obd.myobdapp.Fragments.HomeFragment;
 import com.controller.obd.myobdapp.Fragments.ReadCodeFragment;
 import com.controller.obd.myobdapp.Fragments.TroubleCodesFragment;
 import com.controller.obd.myobdapp.R;
 
+import java.io.IOException;
+import java.util.UUID;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    /* To handle the bluetooth */
+    String BTaddr = null;
+    BluetoothAdapter myBluetooth = null;
+    BluetoothSocket btSocket = null;
+    private boolean isBtConnected = false;
+    static final UUID myUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"); //SPP UUID
+
+    /* To views*/
+    private ProgressDialog progress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +68,25 @@ public class MainActivity extends AppCompatActivity
 
             FragmentManager fragmentManager = getSupportFragmentManager();
             fragmentManager.beginTransaction().replace(R.id.content, fragment).commit();
+        }
+
+        // Checking bluetooth status
+        this.checkBTon();
+    }
+
+    private void checkBTon() {
+        // Handle Bluetooth
+        myBluetooth = BluetoothAdapter.getDefaultAdapter();
+
+        if(myBluetooth == null) // If bluetooth isn't exists
+        {
+            Toast.makeText(this, "Bluetooth Device Not Available", Toast.LENGTH_LONG).show();
+            finish();
+        }
+        else if(!myBluetooth.isEnabled()) // Turning on BT
+        {
+            Intent turnBT_on = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(turnBT_on,1);
         }
     }
 
@@ -157,6 +194,41 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+
+    // Private class to make asynchronous connection
+    private class ConnectBT extends AsyncTask<Void, Void, Void>
+    {
+        private boolean ConnectSuccess = true;
+
+        @Override
+        protected void onPreExecute() {
+            progress = ProgressDialog.show(MainActivity.this, "Connecting...", "Please wait!!!");
+        }
+
+        @Override
+        protected Void doInBackground(Void... devices) {
+            try {
+                if (btSocket == null || !isBtConnected) {
+                    myBluetooth = BluetoothAdapter.getDefaultAdapter();// Get phone BT atention
+                    BluetoothDevice dispositivo = myBluetooth.getRemoteDevice(BTaddr);// Check if the other device is available using it MAC
+                    btSocket = dispositivo.createInsecureRfcommSocketToServiceRecord(myUUID);// Make RF Channel with UUID
+                    BluetoothAdapter.getDefaultAdapter().cancelDiscovery();// Close other RF Channels
+                    btSocket.connect();//start connection
+                }
+            }
+            catch (IOException e) {
+                ConnectSuccess = false;//if the try failed, you can check the exception here
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            progress.dismiss();
+        }
     }
 
 }
